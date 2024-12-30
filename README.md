b853992f60d134a573b62589fe78909778c2e4c1
Mohamed-Moumni <mmoumni@student.1337.ma>
2024-12-30 13:00:14 +0100

    Starting of the project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ft_ping
 Recoding of The Ping Command.
+- [ ] Parsing + Tests
+- [ ] Sending Echo Request + Tests
+- [ ] Recieving Echo Replay + Tests
--- main.c
+++ main.c
@@ -1,16 +1,8 @@
 #include<stdio.h>
 #include<signal.h>
-#include <unistd.h>
-
-void alarm_handler(int signum){
-    printf("Alaram Signal Recieved %d\n", signum);
-}
+#include<unistd.h>
+#include<netinet/ip_icmp.h>
 
 int main(void){
-    signal(SIGALRM, alarm_handler);
-    alarm(5);
-    printf("Alarm Method is Called!!!\n");
-    sleep(10);
-    printf("After the SLEEP METHOD is Called\n");
-    return (0);
+
 }
